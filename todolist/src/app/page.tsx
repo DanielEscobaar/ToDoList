5cf42542361c6c2740230c3bb1cf497dfd4f54c5
DanielEscobaar <descobar@inspalamos.cat>
2023-03-07 16:41:32 +0100

    Update page.tsx

--- todolist/src/app/page.tsx
+++ todolist/src/app/page.tsx
@@ -4,33 +4,10 @@ import styles from './page.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-let newActivity = '';
-
-    interface Act{
-        text: string;
-        status: boolean;
-    }
-	
-	function addToList() 
-    {
-        newActivity.trim();
-		if(newActivity.length > 1){
-            
-            console.log(newActivity);
-            list_act = [...list_act, {text: newActivity, status: false}];
-            newActivity = '';
-        }
-        else alert("No s'ha introduit ningun text");
-	}
-	
-	function removeFromList(act: Act) 
-    {
-        list_act = list_act.filter( (value : Act) => { return value != act } )
-    }
-
 export default function Home() {
 
     
   return (
+    <div></div>
   )
 }
